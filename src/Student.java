4bb2fbbaa9d75a5d27512ccfad727801e294fc5b
Joshua <galbraithjt@gmail.com>
2016-02-28 19:34:09 -0800

    Student Class Constructor, Setters and Getters

--- src/Student.java
+++ src/Student.java
@@ -3,5 +3,35 @@
  */
 public class Student
 {
+    //Declarations
+    private String studentName = "";
+    private String studentNumber="";
 
+    //Full Constructor
+    public Student (String name, String number)
+    {
+        name = studentName;
+        number = studentNumber;
+    }//end full constructor
+
+    //Setters and Getters
+    public String getStudentName()//Student Name getter
+    {
+        return studentName;
+    }
+
+    public void setStudentName(String name)//Student Name setter
+    {
+        studentName = name;
+    }
+
+    public String getStudentNumber()//Student Number getter
+    {
+        return studentNumber;
+    }
+
+    public void setLocationY(String number)//Student Number setter
+    {
+        studentNumber = number;
+    }
 }
